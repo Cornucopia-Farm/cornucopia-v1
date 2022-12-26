87d185caced127a995745f54318d3322e2010da6
shbhakta <shreyabhakta16@gmail.com>
2022-12-25 18:46:19 -0700

    added mobile spacing and column format for mobile bountycard

--- components/bountyCard.tsx
+++ components/bountyCard.tsx
@@ -24,7 +24,8 @@ type Props = {
 
 const BasicCard: React.FC<Props> = props => {
 
-  // const smallScreen = false // useMediaQuery('(max-width: 622px)');
+  const mobileScreen = useMediaQuery('(max-width: 425px)');
+  const largeScreen = useMediaQuery('(min-width: 426px)');
   // const largeScreen = true //useMediaQuery('(min-width: 623px)');
 
   const showLinks = (links: string) => {
@@ -39,9 +40,11 @@ const BasicCard: React.FC<Props> = props => {
     <Card className={styles.cardBackground} sx={{ minWidth: '275', borderRadius: '12px', }}>
       <CardContent>
         <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: '1px', paddingRight: '1px', paddingTop: '1px', }}>
+          {largeScreen && 
+          <> 
           <Box sx={{ display: 'flex', paddingLeft: 0, paddingBottom: 1}}> 
             <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Description </Typography>
-            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%'}}>{props.description}</Typography>
+            <Typography className={styles.cardInfo} sx={{ fontSize: 16, color: '#064829', width: '45%', }}>{props.description}</Typography>
           </Box>
           <Box sx={{ display: 'flex', paddingLeft: 0}}> 
             <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Start Date </Typography>
@@ -57,28 +60,32 @@ const BasicCard: React.FC<Props> = props => {
               {showLinks(props.links)}
             </Typography>
           </Box>
+          </>
+          }
+          {mobileScreen && 
+          <> 
+          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, paddingBottom: 1, }}> 
+            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>Description </Typography>
+            <Typography className={styles.cardInfo} sx={{ fontSize: 16, color: '#064829',  }}>{props.description}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, paddingBottom: 1, }}> 
+            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>Start Date </Typography>
+            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>{dayjs(props.startDate).format('MM-DD-YYYY')}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, paddingBottom: 1,}}> 
+            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>End Date </Typography>
+            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>{dayjs(props.endDate).format('MM-DD-YYYY')}</Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, paddingBottom: 1, }}> 
+            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>Links </Typography>
+            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', overflow: 'auto', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none', },}}>
+              {showLinks(props.links)}
+            </Typography>
+          </Box>
+          </>
+          }
           
-          {/* {smallScreen &&
-          <div> 
-          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, paddingBottom: 1}}> 
-            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Description </Typography>
-            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%'}}>{props.description}</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0}}> 
-            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Start Date </Typography>
-            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%'}}>{dayjs(props.startDate).format('MM-DD-YYYY')}</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0}}> 
-            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>End Date </Typography>
-            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%'}}>{dayjs(props.endDate).format('MM-DD-YYYY')}</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0}}> 
-            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Links </Typography>
-            <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%'}}>{props.links}</Typography>
-          </Box>
-          </div>
-          } */}
-          {props.workLinks &&
+          {largeScreen && props.workLinks &&
             <Box sx={{ display: 'flex', paddingLeft: 0}}> 
               <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Work Links </Typography>
               <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%', overflow: 'auto', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none', }, }}>
@@ -86,16 +93,34 @@ const BasicCard: React.FC<Props> = props => {
               </Typography>
             </Box>
           }
+          {mobileScreen && props.workLinks &&
+            <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, paddingBottom: 1, }}> 
+              <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>Work Links </Typography>
+              <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', overflow: 'auto', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none', }, }}>
+                {showLinks(props.workLinks)}
+              </Typography>
+            </Box>
+          }
+          {largeScreen && 
           <Box sx={{ display: 'flex', paddingLeft: 0}}> 
             <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Arweave Hash </Typography>
             <Typography className={styles.arweaveHash} sx={{fontSize: 16, color: '#064829', width: '45%', overflow: 'auto', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none', }, }}>
               <Link sx= {{ color: 'rgb(233, 233, 198)'}} target="_blank" rel="noopener" href={"https://arweave.net/" + props.arweaveHash}>{props.arweaveHash}</Link>
             </Typography>
           </Box>
+          }
+          {mobileScreen && 
+          <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0, ...(props.finishedStatus?.normalPayout === false && { paddingBottom: 1, }), }}> 
+            <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>Arweave Hash </Typography>
+            <Typography className={styles.arweaveHash} sx={{fontSize: 16, color: '#064829', overflow: 'auto', scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none', }, }}>
+              <Link sx= {{ color: 'rgb(233, 233, 198)'}} target="_blank" rel="noopener" href={"https://arweave.net/" + props.arweaveHash}>{props.arweaveHash}</Link>
+            </Typography>
+          </Box>
+          }
           
-          {props.finishedStatus?.normalPayout === false &&
+          {largeScreen && props.finishedStatus?.normalPayout === false &&
             <Box sx={{ display: 'flex', paddingLeft: 0}}> 
-              <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Outcome </Typography>
+              <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', width: '45%'}}>Outcome</Typography>
               {props.finishedStatus?.creatorRefunded === true &&
                 <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', width: '45%'}}>Creator refunded</Typography>
               }
@@ -113,7 +138,26 @@ const BasicCard: React.FC<Props> = props => {
               }
             </Box>
           }
-        
+          {mobileScreen && props.finishedStatus?.normalPayout === false &&
+            <Box sx={{ display: 'flex', flexDirection: 'column', paddingLeft: 0}}> 
+              <Typography className={styles.cardTag} sx={{ fontSize: 16, color: '#064829', }}>Outcome</Typography>
+              {props.finishedStatus?.creatorRefunded === true &&
+                <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>Creator refunded</Typography>
+              }
+              {props.finishedStatus?.hunterForcePayout === true &&
+                <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>Hunter force payed out</Typography>
+              }
+              {props.finishedStatus?.disputed === true && props.finishedStatus?.creatorWins === true &&
+                <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>Creator won dispute</Typography>
+              }
+              {props.finishedStatus?.disputed === true && props.finishedStatus?.hunterWins === true &&
+                <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>Hunter won dispute</Typography>
+              }
+              {props.finishedStatus?.disputed === true && props.finishedStatus?.tie === true &&
+                <Typography className={styles.cardInfo} sx={{fontSize: 16, color: '#064829', }}>Dispute tied</Typography>
+              }
+            </Box>
+          }
         </Box>
       </CardContent>
       <CardActions>
