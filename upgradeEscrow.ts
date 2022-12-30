b4bc0a77921586bddc5374a465ecfb55d2f04d66
shbhakta <shreyabhakta16@gmail.com>
2022-12-29 22:48:54 -0700

    fixed type error

--- upgradeEscrow.ts
+++ upgradeEscrow.ts
@@ -4,7 +4,7 @@ import escrowABI from './contracts/out/Escrow.sol/Escrow.json';
 
 async function main() {
     const bytecode = ''; // Get contract bytecode by forge inspect Escrow bytecode > ../escrowBytecode.txt and read-in from file
-    const signer = new ethers.Wallet(process.env.DEPLOYER); // set sk for deployer address in env
+    const signer = new ethers.Wallet(process.env.DEPLOYER!); // set sk for deployer address in env
     const escrowFactoryUpgrade = new ethers.ContractFactory(escrowABI['abi'], bytecode, signer);
     const escrowAddress = ''; // Fill this in
 
