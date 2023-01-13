5434c9e7b3c4de2dd5a043e25f66fdab44d8f439
shbhakta <shreyabhakta16@gmail.com>
2023-01-12 21:01:06 -0500

    fixed proxy deploy code and got deployedd on goerli

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -46,6 +46,15 @@ const config: HardhatUserConfig = {
         return line;
       },
     }),
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY!
+  },
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.DEPLOYER!]
+    }
   }
 };
 
--- contracts/upgradeScripts/deployEscrowProxy.ts
+++ contracts/upgradeScripts/deployEscrowProxy.ts
@@ -3,10 +3,10 @@ import { ethers, upgrades } from 'hardhat';
 export async function main() {
     const provider = new ethers.providers.AlchemyProvider('goerli', process.env.ALCHEMY_API_KEY);
     const signer = new ethers.Wallet(process.env.DEPLOYER!, provider); // set sk for deployer address in env
-    const escrowFactory = await ethers.getContractFactory("Escrow", signer);
+    // const escrowFactory = await ethers.getContractFactory("Escrow", signer);
 
+    const escrowFactory = await ethers.getContractFactory("Escrow");
     const escrowContract = await upgrades.deployProxy(escrowFactory, { unsafeAllow: ['delegatecall'], kind: 'uups', }, );
-    await escrowContract.deployed();
     console.log("Escrow deployed to:", escrowContract.address);
 }
 
