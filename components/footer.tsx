63c159863641bf62fe8514c4e131a7e1359898de
atob1 <atoberoff@gmail.com>
2023-03-26 11:44:34 -0400

    fixed typo in about

--- components/footer.tsx
+++ components/footer.tsx
@@ -51,7 +51,7 @@ export default function Footer() {
                       <br />
                       Projects/DAOs can post bounties for freelancers to apply to. Once the bounty creator 
                       accepts the application, the bounty amount will be escrowed in the Cornucopia smart contract. After a freelancer submits their work, the 
-                      creator can then choose to pay freelancer or dispute the bounty. A creator might dispute the bounty
+                      creator can then choose to pay the freelancer or dispute the bounty. A creator might dispute the bounty
                       if they think that the freelancer&apos;s submitted work doesn&apos;t match the bounty description using <Link target="_blank" rel="noopener" href="https://umaproject.org/products/optimistic-oracle">UMAs Optimistic Oracle</Link>. 
                       <br />
                       <br />
@@ -88,16 +88,11 @@ export default function Footer() {
                       <br />
                       Projects/DAOs can post bounties for freelancers to apply to. Once the bounty creator 
                       accepts the application, the bounty amount will be escrowed in the Cornucopia smart contract. After a freelancer submits their work, the 
-                      creator can then choose to pay freelancer or dispute the bounty. A creator might dispute the bounty
-                      if they think that the freelancer&apos;s submitted work doesn&apos;t match the bounty description. 
+                      creator can then choose to pay the freelancer or dispute the bounty. A creator might dispute the bounty
+                      if they think that the freelancer&apos;s submitted work doesn&apos;t match the bounty description using <Link target="_blank" rel="noopener" href="https://umaproject.org/products/optimistic-oracle">UMAs Optimistic Oracle</Link>. 
                       <br />
                       <br />
-                      The freelancer then has 1 week to respond to the dispute: they can 
-                      either dispute the creator&apos;s dispute or leave it and loose any chance of recuperating some of the bounty amount. If they choose to dispute, then the dispute 
-                      is escalated to UMA token holders (using <Link sx= {{ color: 'rgb(233, 233, 198)', }} target="_blank" rel="noopener" href="https://umaproject.org/products/optimistic-oracle">UMAs Optimistic Oracle</Link>) who then vote whether they think the freelancer did the work, did not do the work, or unclear whether the freelancer did the work.
-                      In the first case, the freelancer gets paid the bounty amount plus half the bond the creator put up to dispute the work. In the second case, the creator gets their 
-                      escrowed funds back plus half the bond the freelancer put up to dispute the creator&apos;s dispute. In the third case, the freelancer gets paid half the bounty amount and 
-                      half the bond the creator put up to dispute the work while the creator gets half the bount amount back. 
+                      Read more about the protocol in depth in our <Link target="_blank" rel="noopener" href="https://docs.cornucopia.farm">docs</Link> and join our community on <Link target="_blank" rel="noopener" href="https://discord.gg/KxCgGceA6M">discord.</Link>
                       <br />
                       <br />
                       We hope you enjoy and happy hunting! 
